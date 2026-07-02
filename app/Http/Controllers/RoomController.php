2cc0b09eccc5ad320dbc0317bba6c7f1b9f26422
radovoahary <voaharyradoniaina@gmail.com>
2026-07-02 18:01:19 +0300

    debugging

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -2,15 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers;
 use App\Models\Room;
 use Inertia\Inertia;
+use Illuminate\Http\Request;
 
-class RoomController extends Controller {
+class RoomController extends Controller 
+{
     public function index()
     {
-        //Récupération de toutes les chambres de la base de données
-        $room = Room::all();
+        // Récupération de toutes les chambres de la base de données
+        $rooms = Room::all();
         
+        // On les envoie au composant React "Rooms/Index"
+        return Inertia::render('Rooms/Index', [
+            'rooms' => $rooms
+        ]);
     }
 }
