bd7eb7a933aa76b96eb6fb51c32824f295b86b72
radovoahary <voaharyradoniaina@gmail.com>
2026-07-05 10:26:21 +0300

    Adding methods to ClientController

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Client;
+use Inertia\Inertia;
 use Illuminate\Http\Request;
 
 class ClientController extends Controller
 {
-    //
+    public function index()
+    {
+        return Inertia::render('Client/Index', [
+            'clients' => Client::all()
+        ]);
+    }
 }
