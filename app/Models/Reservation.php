3611190c46aba8117235ef7475a0c82b57952976
radovoahary <voaharyradoniaina@gmail.com>
2026-07-05 13:51:32 +0300

    Relation between client-rooms etablished

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reservation extends Model
 {
-    //
+    protected $fillable = ['client_id', 'room_id', 'check_in_date', 'check_out_date', 'total_price', 'status'];
+
+    //Relation avec le client
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
+
+    //Relation avec la chambre
+    public function room()
+    {
+        return $this->belongsTo(Room::class);
+    }
 }
